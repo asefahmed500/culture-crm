3d606651320f176700fade9db8cad424b4f8f02d
Asef Ahmed <asefahmedcs@gmail.com>
2025-07-27 08:36:34 +0000

    look the css is not woring

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -1,13 +1,12 @@
-
 import type {Config} from 'tailwindcss';
 import {fontFamily} from 'tailwindcss/defaultTheme';
 
 const config: Config = {
   darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     container: {
