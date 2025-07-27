283acd3a7c7b271fbbe161fdca62218b88d87928
Asef Ahmed <asefahmedcs@gmail.com>
2025-07-27 09:18:13 +0000

    still not fixded vcheck again

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,13 @@
+
 import type {Config} from 'tailwindcss';
 import {fontFamily} from 'tailwindcss/defaultTheme';
 
 const config: Config = {
   darkMode: ['class'],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     container: {
