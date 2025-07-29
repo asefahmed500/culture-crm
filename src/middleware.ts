672ef53b0cc025fb309074c7bbea9fae2d0a9284
Asef Ahmed <asefahmedcs@gmail.com>
2025-07-29 15:14:35 +0000

    first show the homepage when the app start

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,5 +5,5 @@ export const config = {
     // The following routes are defined as public routes.
     // Please note that this is a simplified regex and may need to be updated.
     // For more information, see: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-    matcher: ['/((?!login|signup|_next/static|_next/image|favicon.ico).*)'] 
+    matcher: ['/((?!api/|login|signup|_next/static|_next/image|favicon.ico|$).*)']
 };
