0fcabbd634e904ae17e9da1fd594c0f895c6ff87
Asef Ahmed <asefahmedcs@gmail.com>
2025-07-29 07:45:46 +0000

    make sure ther is no dupliacate files whch could cause errros

--- src/app/(public)/layout.tsx
+++
@@ -1,8 +0,0 @@
-// This layout is for public pages that should not have the AppShell.
-export default function PublicLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return children;
-}
