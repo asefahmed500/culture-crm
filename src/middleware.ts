87491310585bde75f4dd92cff7d17ca2ec3025ab
Asef Ahmed <asefahmedcs@gmail.com>
2025-07-29 15:18:27 +0000

    GET /login?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2F 200 in 5210ms  ✓

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
+
 // src/middleware.ts
 export { default } from "next-auth/middleware"
 
@@ -5,5 +6,5 @@ export const config = {
     // The following routes are defined as public routes.
     // Please note that this is a simplified regex and may need to be updated.
     // For more information, see: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-    matcher: ['/((?!api/|login|signup|_next/static|_next/image|favicon.ico|$).*)']
+    matcher: ['/((?!api/auth|login|signup|_next/static|_next/image|favicon.ico|$).*)']
 };
